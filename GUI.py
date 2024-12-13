649c911065cbae899c405fd731501b747712d070
vikiaseschool <vyhnalekv@ceskolipska.cz>
2024-12-13 22:57:38 +0100

    Update GUI.py

--- GUI.py
+++ GUI.py
@@ -154,6 +154,6 @@ def submit_feedback():
     return jsonify({"message": "Feedback submitted successfully!"})
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5001)
+    app.run(host='0.0.0.0', port=8544)
 
 
